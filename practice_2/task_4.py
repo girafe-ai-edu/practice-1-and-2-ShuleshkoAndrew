6f08228ebc593f78a35d82e8085285f965469e2f
ShuleshkoAndrew <shuleshko.av@phystech.edu>
2024-09-23 15:51:30 +0300

    --

--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -2,7 +2,7 @@
 """
 Develop a program that asks the user for an integer 4-digit number and calculates the sum of its constituent digits. For example, if the user enters the number 3141, the program should output the following result:
 3 + 1 + 4 + 1 = 9
-
+and make commit...
 """
 def four_sum():
     print('Введите четырехзначное число')
